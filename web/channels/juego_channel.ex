02f62797031bbbb209a5dedf1bcad332112bffb0
id-fga <investigacion.desarrollo.fga@gmail.com>
2016-04-16 23:07:11 -0300

    Se asignan valores al canal

--- web/channels/juego_channel.ex
+++ web/channels/juego_channel.ex
@@ -1,14 +1,24 @@
 defmodule LaGuerraDeLosLados.JuegoChannel do
   use Phoenix.Channel
+  alias Phoenix.Socket
 
-  #TODO: Hacer un chequeo de verdad de la cantidad de jugadores
-  #TODO: Implementar proceso aparte para mantener el estado
+  #TODO: Hacer un chequeo de verdad de la cantidad de jugadores.
+  #TODO: Implementar proceso aparte para mantener el estado.
+  #TODO: La variable en params salaNombre, sobra.
   def join("juego:" <> salaNombre, params, socket) do
-    case params["jugadorNumero"] do
+    jugador_sala = params["salaNombre"]
+    jugador_nombre = params["jugadorNombre"]
+    jugador_numero = params["jugadorNumero"]
+
+    case jugador_numero do
       "jugador2"  ->  IO.puts "Sala #{inspect salaNombre} lista"
       _           ->  true
     end
-    {:ok, socket}
+    {:ok, socket
+          |> Socket.assign(:jugador_sala, jugador_sala)
+          |> Socket.assign(:jugador_nombre, jugador_nombre)
+          |> Socket.assign(:jugador_numero, jugador_sala)
+    }
   end
 
 end
