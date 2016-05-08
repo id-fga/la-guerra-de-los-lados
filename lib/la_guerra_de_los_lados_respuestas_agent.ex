752ce50a61fdaa0804bee9604b4e18efb780fdd2
id-fga <investigacion.desarrollo.fga@gmail.com>
2016-05-08 00:46:43 -0300

    Se reimplementa Agente para manejar manos

--- lib/la_guerra_de_los_lados_respuestas_agent.ex
+++ lib/la_guerra_de_los_lados_respuestas_agent.ex
@@ -1,20 +1,49 @@
 defmodule LaGuerraDeLosLados.Respuestas do
   def start_link do
-    Agent.start_link(fn -> %{} end, name: __MODULE__)
+    Agent.start_link(fn-> %{} end, name: __MODULE__)
+  end
+  
+  def crear_sala(sala_nombre) do
+    mapa = %{
+              jugador1: "",
+              jugador2: "",
+              puntaje_jugador1: 0,
+              puntaje_jugador2: 0,
+              guerra: 0,
+              respuestas: []
+    }
+
+    Agent.update(__MODULE__, fn todas -> Map.put(todas, sala_nombre, mapa) end)
   end
 
-  def traer_sala(sala) do
-    Agent.get(__MODULE__, fn todos -> Map.get(todos, sala) end)
+  def agregar_jugador(sala_nombre, numero_jugador, nombre_jugador) do
+    todas = traer_todas
+    sala_actual = Map.get(todas, sala_nombre)
+
+
+    nueva_info = case numero_jugador do
+      "jugador1"  ->  nueva_info = %{ sala_actual | jugador1: nombre_jugador }
+
+      "jugador2"  ->  nueva_info = %{ sala_actual | jugador2: nombre_jugador }
+
+    end
+
+    Agent.update(__MODULE__, fn todas -> Map.put(todas, sala_nombre, nueva_info) end)
+
   end
 
-  def agregar_sala(sala) do
-    Agent.update(__MODULE__, fn todos -> Map.put(todos, sala, []) end)
+  def agregar_respuestas(sala_nombre, respuestas) do
+    #[{"jugador1", "jugador1"}, {"jugador2", "jugador1"}]
+    todas = traer_todas
+    sala_actual = Map.get(todas, sala_nombre)
+
+    nuevas_respuestas = sala_actual[:respuestas] ++ respuestas
+    nueva_info = %{ sala_actual | respuestas: nuevas_respuestas }
+    Agent.update(__MODULE__, fn todas -> Map.put(todas, sala_nombre, nueva_info) end)
   end
 
-  def agregar_respuestas(sala, respuestas) do
-    s = traer_sala(sala)
-    ns = s ++ [respuestas]
-    Agent.update(__MODULE__, fn todos -> Map.put(todos, sala, ns) end)
+  def traer_todas do
+    Agent.get(__MODULE__, fn todas -> todas end)
   end
 
 end
